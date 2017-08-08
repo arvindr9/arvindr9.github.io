72256e974341a8b1356c7ad93255f476b1beff7e
arvindr9 <saramaswami@gmail.com>
2017-08-07 20:41:54 -0400

    Added a project!
    I made a penny image that can be dragged using JS and CSS animations.

---
+++ drag.js
@@ -0,0 +1,31 @@
+let dragStart = false;
+let originX = null;
+let offsetX = 0;
+let offsetY = 0;
+
+function onDragStart(event){
+	event.preventDefault();
+	originX = event.clientX;
+	originY = event.clientY;
+	dragStart = true;
+}
+function onDragMove(event){
+	if(!dragStart){
+		return;
+	}
+	const currentX = event.clientX;
+	const deltaX = currentX-originX;
+	const translateX = offsetX + deltaX;
+	const currentY = event.clientY;
+	const deltaY = currentY-originY;
+	const translateY = offsetY + deltaY;
+	event.currentTarget.style.transform = 'translate(' + translateX + 'px, ' + translateY + 'px)';
+}
+function onDragEnd(event){
+	dragStart = false;
+}
+
+const penny = document.querySelector('#penny');
+penny.addEventListener('pointerdown', onDragStart);
+penny.addEventListener('pointermove', onDragMove);
+penny.addEventListener('pointerup', onDragEnd);
